dc2252bd9d2d0963e4b17cf3426afeff71298b78
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-09-11 16:16:01 +0200

    Initial Commit

---
+++ ResponsiveUINew/ResponsiveUINewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUINewApp.swift
+//  ResponsiveUINew
+//
+//  Created by Michele Manniello on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUINewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
